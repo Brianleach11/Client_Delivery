7f74f55ef8bb2bb0822fdcbe8449bc4fc951798e
brianleach11 <bridog314@gmail.com>
2023-05-13 01:01:35 -0400

    Adding in directions services and renderers in order to get directions between the optimal delivery routes

--- src/main/java/com/google/maps/index.js
+++ src/main/java/com/google/maps/index.js
@@ -1,14 +1,11 @@
 let map;
 
-async function initMap() {
+function initMap() {
     // The location of Uluru
     const position = { lat: 26.0797377, lng: -80.27373039 };
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
 
     // The map, centered at Uluru
-    map = new Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
         center: position,
         mapId: "DEMO_MAP_ID",
@@ -18,6 +15,7 @@ async function initMap() {
 
     trafficLayer.setMap(map);
 }
+
 async function getOptimizedRoute(addresses) {
     try {
         const response = await fetch("http://localhost:8080/optimize-route", {
@@ -41,9 +39,6 @@ async function handleSubmit(event) {
     event.preventDefault();
     console.log("Handling the submit Button");
 
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const { LatLng } = await google.maps.importLibrary("core");
-
     // Get the value of the addresses input field
     const addresses = document.getElementById("inputAddresses").value.split(",");
     // Call getOptimizedRoute with the addresses value
@@ -51,20 +46,47 @@ async function handleSubmit(event) {
         let jsonData = await getOptimizedRoute(addresses);
         const latLngList = Object.values(jsonData);
 
-        for (let item of latLngList) {
-            var newMarker = new AdvancedMarkerElement({
-                map: map,
-                position: item
-            });
-        }
+        let directionsService = new google.maps.DirectionsService();
+        let directionsRenderer = new google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(map);
+
+        let waypoints = latLngList.slice(1, latLngList.length - 1).map(({ lat, lng }) => ({
+            location: { lat, lng },
+            stopover: true
+        }));
+
+        let request = {
+            origin: latLngList[0], // Start
+            destination: latLngList[latLngList.length - 1], // End
+            waypoints: waypoints, // Waypoints
+            travelMode: 'DRIVING' // Travel mode
+        };
+
+        directionsService.route(request, function(result, status) {
+            if (status == 'OK') {
+                // Set the directions on the map
+                directionsRenderer.setDirections(result);
+
+                // Get the route's bounds
+                var bounds = new google.maps.LatLngBounds();
+                var route = result.routes[0];
+                for (var i = 0; i < route.legs.length; i++) {
+                    bounds.extend(route.legs[i].start_location);
+                    bounds.extend(route.legs[i].end_location);
+                }
+
+                // Adjust the map's viewport to fit the entire route
+                map.fitBounds(bounds);
+            }
+        });
     } catch (error) {
         console.error(error);
     }
-
 }
 
-initMap();
-
 // Add an event listener to the form element to listen for the submit event and call the handleSubmit function
-const form = document.getElementById("myForm");
-form.addEventListener("submit", handleSubmit);
+window.onload = function() {
+    initMap();
+    const form = document.getElementById("myForm");
+    form.addEventListener("submit", handleSubmit);
+}
